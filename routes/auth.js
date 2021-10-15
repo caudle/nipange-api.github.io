4618eb224738a62dafe2bf7001785ab23d0a817e
caudle <lemaemanuel96@gmail.com>
2021-10-15 13:28:20 +0300

    fix bugs

--- routes/auth.js
+++ routes/auth.js
@@ -127,7 +127,7 @@ router.get('/forgotPassword/:email', async (req, res) => {
   // try to send email
   try {
     const transporter = nodemailer.createTransport({
-      host: 'mail.nipange.com',
+      host: '162.214.100.22',
       
       secure: true,
       port: 465,
